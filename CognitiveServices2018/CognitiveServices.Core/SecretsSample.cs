30756a8275f47e40ade4a8626c9eed483b6d9bd0
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2018-12-03 17:03:47 +0900

    Add sample file of secret keys

---
+++ CognitiveServices2018/CognitiveServices.Core/SecretsSample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CognitiveServices.Core
+{
+    //public class Secrets
+    //{
+    //    public const string FaceApiKey = "<Face API Key>";
+    //    public const string ComputerVisionApiKey = "<Computer Vision API Key>";
+    //    // Please replace to properlly region you use.
+    //    // If you are using Trial, please use "westcentralus".
+    //    public const string FaceEndpoint = "https://southeastasia.api.cognitive.microsoft.com";
+    //    public const string ComputerVisionEndpoint = "https://southeastasia.api.cognitive.microsoft.com/";
+    //}
+}
